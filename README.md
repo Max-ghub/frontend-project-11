00e17a684a240e311c678e4fc19a248efbd8efb1
Max <max@mail.ru>
2022-11-05 18:59:09 +0500

    init project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Max-ghub/frontend-project-11/workflows/hexlet-check/badge.svg)](https://github.com/Max-ghub/frontend-project-11/actions)
+[![Actions Status](https://github.com/Max-ghub/frontend-project-11/workflows/linter-check/badge.svg)](https://github.com/Max-ghub/frontend-project-11/actions)
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+  entry: './src/index.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    filename: 'index.js',
+  },
+
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = 'production';
+
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = 'development';
+  }
+  return config;
+};
